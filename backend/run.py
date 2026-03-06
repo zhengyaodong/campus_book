2f66e705884ae2ca334d2b606aaf7eb14e21cb7a
zhengyaodong <zhengyaodong@gmail.com>
2026-03-06 22:01:26 +0800

    fix: backend add flask_cors

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,12 @@
 # 应用入口
 from app import create_app
+from flask_cors import CORS  # 1. 导入 CORS
 
 app = create_app()
+CORS(app, supports_credentials=True) # 2. 核心：允许跨域请求
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    # 建议使用系统分配的端口，增加稳定性
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
